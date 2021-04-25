4862da7cec557bcfa236740faecf07d1605bf831
Sung-Hwi <75319719+halo-wi@users.noreply.github.com>
2021-04-25 22:31:03 +0900

    Create 백준알고리즘 3단계-10.java

---
+++ 백준알고리즘 3단계-10.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		for (int i = 0; i < n; i++) {
+			for(int j = n-1; j-i > 0; j--) {
+				bw.write(" ");
+			}
+			for(int j = 0; j < i+1; j++) {
+				bw.write("*");
+			}			
+			bw.write("\n");
+		}
+		bw.flush();
+		br.close();
+	}
+}

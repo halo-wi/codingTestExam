e62ec07e82cdaa78d871c37d0a4f134d71801da1
Sung-Hwi <75319719+halo-wi@users.noreply.github.com>
2021-04-25 22:29:45 +0900

    Create 백준알고리즘 3단계-8.java

---
+++ 백준알고리즘 3단계-8.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		for (int i = 0; i < n; i++) {
+			String s = br.readLine();
+			int index = s.indexOf(" ");
+			int a = Integer.parseInt(s.substring(0, index));
+			int b = Integer.parseInt(s.substring(index + 1));
+			bw.write("Case #"+String.valueOf(i + 1)+": "+a+" + "+b+" = ");
+			bw.write(String.valueOf(a + b));
+			bw.write("\n");
+		}
+		bw.flush();
+		br.close();
+	}
+}

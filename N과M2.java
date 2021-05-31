ba301d119c391a7efd97181a65762b844b65b7f0
Sung-Hwi <75319719+halo-wi@users.noreply.github.com>
2021-05-31 20:27:51 +0900

    Create N과M2.java

---
+++ N과M2.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int n =0;
+	static int m =0;
+	static int[] arr;
+	static boolean[] exp;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n=Integer.parseInt(st.nextToken());
+		m=Integer.parseInt(st.nextToken());
+		arr=new int[m];
+		exp=new boolean[n];
+		dfs(0);
+		System.out.println(sb);
+	}
+
+	private static void dfs(int dep) {
+		if(dep==m) {
+			for(int i:arr) {
+			sb.append(i).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+		for(int i=0; i<n; i++) {
+			if(!exp[i]) {
+				if (dep>0 && arr[dep-1]>=i+1) 
+					continue;
+				exp[i]=true;
+				arr[dep]=i+1;
+				dfs(dep+1);
+				exp[i]=false;
+			}
+		}
+	}
+}

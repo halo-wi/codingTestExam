fcd0270d9def927685c9d729b986851678612a84
Sung-Hwi <75319719+halo-wi@users.noreply.github.com>
2021-06-06 22:49:01 +0900

    Create 백준정렬.java

---
+++ 백준정렬.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Arrays;
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int a = Integer.parseInt(br.readLine());
+		int[] arr=new int[a];
+		for(int i=0; i<a; i++) {
+			arr[i]=Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		for(int i=0; i<a; i++) {
+			bw.write(arr[i]+"\n");
+		}
+		bw.flush();
+	}
+}
